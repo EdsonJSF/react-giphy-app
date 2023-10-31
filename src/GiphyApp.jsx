254e0e6862695d284438ca506e85d7d3de61ecc6
EdsonJSF <EdsonJSF1990@gmail.com>
2023-10-30 21:22:37 -0400

    Feat: Categories list

--- src/GiphyApp.jsx
+++ src/GiphyApp.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
+
 export const GiphyApp = () => {
+  const [categories, setCategories] = useState(["One Ounch", "Edson"]);
+
+  console.log(categories);
+
   return (
     <>
       {/* Title */}
-      <h1>{GiphyApp}</h1>
+      <h1>GiphyApp</h1>
 
       {/* input */}
 
       {/* Gifs list */}
         {/* Gif item */}
+
+      {/* Categories list */}
+      <ol>
+        {/* Category item */}
+        {categories.map((category) => {
+          return <li key={category}>{category}</li>;
+        })}
+      </ol>
     </>
   );
 };
