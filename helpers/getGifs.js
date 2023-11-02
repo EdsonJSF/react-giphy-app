d9fed7bee105cd701e6e68e2678ccf2edca37cbf
EdsonJSF <EdsonJSF1990@gmail.com>
2023-11-02 12:24:32 -0400

    Feat: getGifs

---
+++ helpers/getGifs.js
@@ -0,0 +1,13 @@
+import { GIPHY_TOKEN } from "../env";
+
+export const getGifs = async (category, limit = 10) => {
+  const url = `https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_TOKEN}&q=${category}&limit=${limit}`;
+  const resp = await fetch(url);
+  const { data } = await resp.json();
+  const gifs = data.map((gif) => ({
+    id: gif.id,
+    title: gif.title,
+    url: gif.images.downsized_medium.url,
+  }));
+  console.log(gifs);
+};
